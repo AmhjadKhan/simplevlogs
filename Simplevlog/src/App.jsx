62d64359b9a1537ede47b5d7b390e30c7077670d
Amhjadkhan <amjadpogramer@gmail.com>
2023-09-12 23:22:15 +0400

    first commit

---
+++ Simplevlog/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Blogs from './Components/Blogs/Blogs'
+
+const App = () => {
+  return (
+    <div>
+      <h1>how are you</h1>
+      <Blogs />
+    </div>
+  )
+}
+
+export default App
